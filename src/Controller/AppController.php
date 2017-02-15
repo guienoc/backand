754b0d07bf7e2c4333156ea1ca620428f8b538d1
Luciano Cantarelli <lucianocantarelli.com.br@gmail.com>
2017-02-15 00:45:44 -0200

    app merge

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -23,12 +23,7 @@ class AppController extends Controller
 
     public function beforeRender(Event $event)
     {
-<<<<<<< HEAD
-        
-=======
         $auth_user = $this->Auth->user();
-
->>>>>>> cf25a91f4c3a984046c11bc8ab59c8399707953d
         $base = 'http://' . getenv('HTTP_HOST');
         $controller = $this->request->controller;
         $action = $this->request->action;
